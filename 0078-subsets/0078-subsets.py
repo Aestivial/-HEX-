8040dbe48befdfa615514c90b5837458d0cdc195
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2023-07-17 22:41:58 +0530

    Time: 52 ms (43.95%), Space: 16.6 MB (47.74%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,6 @@
 class Solution:        
     def subsets(self, nums: List[int]) -> List[List[int]]:
         power_sets=[[]]
-        
         for num in nums:
             power_sets+=[subset+[num] for subset in power_sets]
-        
         return power_sets
